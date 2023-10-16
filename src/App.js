4a4c2f2cd85c1b20076848d88ac0d907da9a6429
Mohsin <mdmohsinhaider6@gmail.com>
2023-10-17 00:32:11 +0530

    upload_1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react'
 function App() {
   return (
     <div className="App">
+      <h2 className='h'>Tic Tac Toe Game Note Pad</h2>
       <Board></Board>
     </div>
   );
